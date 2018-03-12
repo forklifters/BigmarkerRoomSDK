4493a0453888490c9a892a993dff5e9cec5fb7f0
hanqing <qing.han@bigmarker.com>
2018-03-12 13:55:47 +0800

    Initial Commit

---
+++ BigmarkerRoomSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BigmarkerRoomSDK
+//
+//  Created by Han Qing on 12/3/2018.
+//  Copyright © 2018 bigmarker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
